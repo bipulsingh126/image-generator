284c6f35f8b0dc15165bc5a9ee1673ee7186a459
Bipul Pratap Singh <156681123+bipulsingh126@users.noreply.github.com>
2025-01-16 23:32:57 +0530

    No changes detected in the files. Commit is empty.

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./Pages/Home.jsx";
+import BuyCredit from "./Pages/BuyCredit";
+import Result from "./Pages/Result";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-10 md:px-40 lg:px-28 min-h-screen bg-gradient-to-b from-[#9cc1c8] to-[#d2c292]  ">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy-credit" element={<BuyCredit />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
